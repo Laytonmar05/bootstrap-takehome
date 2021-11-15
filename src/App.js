8b6dfb22313035b399842b91b2c2bf6d6b10f47b
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2021-11-15 00:23:08 -0800

    Forms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import "./App.css";
 
@@ -7,7 +8,26 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <p>Hello World!</p>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="name" placeholder="Enter Name" />
+            <Form.Text className="text-muted">
+              Your name will only stay with us.
+    </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="me@email.com" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+  </Button>
+        </Form>
         <Card style={{ width: '18rem' }}>
           <Card.Img variant="top" src="https://www.lifewire.com/thmb/-MWE5VJWnlMWf3wrvBmE34AcRt4=/3863x2578/filters:fill(auto,1)/the-bund-of-shanghai-reflected-in-a-magnifying-glass-657283468-5a2f09be98020700374eaa1b.jpg" />
           <Card.Body>
